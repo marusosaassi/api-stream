f2d031ca91cdf48b6c1f72054332aa5177f9072b
Maru Sosa <soassi@outlook.com>
2022-10-14 07:30:18 -0500

    refactor

--- src/api/stream/examples/StreamDistinctExample.java
+++ src/api/stream/examples/StreamDistinctExample.java
@@ -1,11 +1,8 @@
 package api.stream.examples;
 
-import api.stream.examples.models.User;
-
-import java.util.Optional;
 import java.util.stream.Stream;
 
-public class StreamSingleDistinctExample {
+public class StreamDistinctExample {
     public static void main(String[] args) {
 
         /*Stream.of("Rafael Flores", "Maru Sosa", "Damian Ayim", "Victor Lavalle",
